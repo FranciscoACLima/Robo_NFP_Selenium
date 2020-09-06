7f25eeff9bb20909383c7e4adb276360abccb78e
Francisco A C Lima <franciscoaclima@outlook.com>
2020-09-06 11:44:33 -0300

    Método para configurar cadastro da nota fiscal

--- tests_nfp.py
+++ tests_nfp.py
@@ -2,9 +2,9 @@ import os
 from nfp.telas.nfp import Nfp
 
 
-def gravar_nota(cod_nota, usuario, senha, driver=None):
+def gravar_nota(cod_nota, usuario, senha, mes, ano, entidade, driver=None):
     retorno = ''
-    tela = Nfp(cod_nota, usuario, senha, driver)
+    tela = Nfp(cod_nota, usuario, senha, mes, ano, entidade, driver)
     tela.default_sleep = 5
     if not driver:
         retorno = tela.abrir_pagina_login()
@@ -13,6 +13,7 @@ def gravar_nota(cod_nota, usuario, senha, driver=None):
         retorno = tela.logar()
         if retorno:
             return retorno, None
+        tela.configurar_cadastro()
     return retorno, driver
 
 
@@ -20,5 +21,8 @@ if __name__ == "__main__":
     cod_nota = 'xxxxxxxxxxxx'
     usuario = os.environ['USER_NFP']
     senha = os.environ['SENHA_NFP']
-    r, d = gravar_nota(cod_nota, usuario, senha)
+    mes = '08'
+    ano = '2020'
+    entidade = 'CREN - CENTRO DE RECUPERAÇÃO E EDUCAÇÃO NUTRICIONAL'
+    r, d = gravar_nota(cod_nota, usuario, senha, mes, ano, entidade)
     print(r)
